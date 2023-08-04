968a9aed463bbeeaf7a2216b98c57616e803b0e8
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-08-05 01:22:51 +0300

    Update Main.java

--- CLI/src/Main.java
+++ CLI/src/Main.java
@@ -1,14 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Terminal t = new Terminal();
         Scanner input = new Scanner(System.in);
         String userInput;
         while (true) {
             System.out.print("> ");
             userInput = input.nextLine();
-            if(t.parser.parse(userInput)){
-                t.chooseCommandAction();
+            String[] commands = userInput.split(" & ");
+            for(int i=0; i<commands.length; i++) {
+                if (t.parser.parse(commands[i])) {
+                    t.chooseCommandAction();
+                }
             }
         }
     }
