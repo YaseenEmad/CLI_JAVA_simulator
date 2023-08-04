16fe2a41ae4f030fa09bc656f55415c606199283
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-08-04 19:42:15 +0300

    Update Parser.java

--- CLI/src/Parser.java
+++ CLI/src/Parser.java
@@ -3,10 +3,28 @@ public class Parser {
     String[] args;
 
     //This method will divide the input into commandName and args, where "input" is the string command entered by the user
-    //It also checks validation of the input/number of args if valid --> return true, if not --> prints error message then return false
+    //It also checks validation of the command name/number of args if valid --> return true, if not --> prints error message then return false
     public boolean parse(String input){
-        
-    }
+        String[] tempArr = input.split(" ");
+        commandName = tempArr[0];
+        String[] myArgs = new String[tempArr.length-1];
+        for(int i = 1; i<tempArr.length; i++){
+            myArgs[i-1] = tempArr[i];
+        }
+        args = myArgs;
+
+//        switch(commandName) {
+//            case "cd":
+//                if(args.length > 1){
+//                    System.out.println("error");
+//                    return false;
+//                }
+//
+//                defualt:
+//                System.out.println("invalid command name");
+//        }
+
+        return true;} //{cp(commandName), file1.txt, file2.txt }
     public String getCommandName(){
         return commandName;
     }
