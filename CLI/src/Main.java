bdb8ad210b3d631c66479970380e243d967d2eaa
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-08-04 19:38:10 +0300

    Update Main.java

--- CLI/src/Main.java
+++ CLI/src/Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Terminal t = new Terminal();
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            System.out.print("> ");
+            userInput = input.nextLine();
+            if(t.parser.parse(userInput)){
+                t.chooseCommandAction();
+            }
+        }
     }
 }
